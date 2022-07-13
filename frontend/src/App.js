dc25836d5895cc88b595ce6a02fe3206b3d3a7f6
Marwan Abdelsattar <marwaneg@aucegypt.edu>
2022-07-13 21:48:12 +0200

    eslint and prettier setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,32 +3,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
-
-const UNSPLASH_KEY = "t11X0zZNEoDJNqc7e3M0Uj3RIV37FQAzf5g9yhk9k8U";
+const UNSPLASH_KEY = 't11X0zZNEoDJNqc7e3M0Uj3RIV37FQAzf5g9yhk9k8U';
 
 function App() {
-  const [word, setWord] = useState(''); 
-
-  const handleSearchSubmit = (e) =>{
+  const [word, setWord] = useState('');
+  const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
-      .then((res)=>res.json())
-      .then((data)=>{
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
         console.log(data);
       })
-      .catch((err)=>{
+      .catch((err) => {
         console.log(err);
-      })
-      setWord('');
-  }
+      });
+    setWord('');
+  };
   // console.log(UNSPLASH_KEY);
- 
 
   return (
     <div className="App">
-      <Header title = "Images Gallery"/>
-      <Search word = {word} setWord = {setWord} handleSubmit={handleSearchSubmit}/>
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
