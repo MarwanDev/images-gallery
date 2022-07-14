b0fe61c1f806cd0f3ca4da29531b66a78bd1f0c8
Marwan Abdelsattar <marwaneg@aucegypt.edu>
2022-07-14 21:26:54 +0200

    added text logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
+import { ReactComponent as Logo } from '../images/logo.svg';
 const navbarStyle = {
   backgroundColor: 'lightblue',
 };
@@ -7,7 +8,7 @@ const Header = (props) => {
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container>
-        <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+        <Logo style={{ maxWidth: '12rem', maxHeight: '2rem' }} />
       </Container>
     </Navbar>
   );
