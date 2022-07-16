0ef750573fb0d4176c126e604217fa5defc034f2
Marwan Abdelsattar <marwaneg@aucegypt.edu>
2022-07-16 23:24:23 +0200

    creating new image flask API endpoint

--- api/main.py
+++ api/main.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from urllib import response
+import requests 
+from flask import Flask, request
+
+UNSPLASH_URL="https://api.unsplash.com/photos/random"
+UNSPLASH_KEY="t11X0zZNEoDJNqc7e3M0Uj3RIV37FQAzf5g9yhk9k8U"
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/new-image")
+def new_image():
+   word = request.args.get("query")
+   headers={
+    "Accept-Version":"v1",
+    "Authorization":"Client-ID " + UNSPLASH_KEY
+   }
+   params = {
+    "query":word
+   }
+   response = requests.get(UNSPLASH_URL, headers=headers, params=params)
+   data=response.json()
+   return {"data":data} 
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=5050)
